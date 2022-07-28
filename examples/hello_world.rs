8af5b9322a815df9bd4ec503552df317c5d88aa8
dAxpeDDa <daxpedda@gmail.com>
2022-07-28 03:10:44 +0200

    Improve example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,9 +1,5 @@
-//! Run with `cargo run --example hello_world` command.
-//!
-//! To connect through browser, navigate to "http://localhost:3000" url.
-
 use anyhow::Result;
-use axum::{routing::get, Router};
+use axum::{routing, Router};
 use axum_server::tls_rustls::RustlsConfig;
 use axum_server_upgrade_http::UpgradeHttpLayer;
 use std::net::SocketAddr;
@@ -11,13 +7,13 @@ use std::net::SocketAddr;
 #[tokio::main]
 async fn main() -> Result<()> {
     let app = Router::new()
-        .route("/", get(|| async { "Hello, world!" }))
+        .route("/", routing::get(|| async { "Hello, world!" }))
         .layer(UpgradeHttpLayer);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on {}.", addr);
     println!(
-        "Connecting to \"http://{}\" with any path or query will automatically redirect to \"https://{0}\".",
+        "Connecting to \"http://{}\" with any path or query will automatically redirect to \"https://{0}\" with the same path and query.",
         addr
     );
 
@@ -30,8 +26,7 @@ async fn main() -> Result<()> {
 
     axum_server_upgrade_http::bind_dual(addr, config)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
 
     Ok(())
 }
